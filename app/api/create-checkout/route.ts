134cbe3726101cf4d2cf99b6c88c04049fcaee6d
alidow <alidow@gmail.com>
2025-08-01 20:06:50 -0400

    Update Stripe API version to fix build error

--- app/api/create-checkout/route.ts
+++ app/api/create-checkout/route.ts
@@ -4,7 +4,7 @@ import { supabaseAdmin } from "@/lib/supabase"
 import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16"
+  apiVersion: "2025-07-30.acacia"
 })
 
 export async function POST(req: NextRequest) {
