8973195a3c7e91fb95a89fd9d9e58a93217adb07
alidow <alidow@gmail.com>
2025-08-01 20:16:43 -0400

    Fix Stripe API version to use basil

--- app/api/create-portal-session/route.ts
+++ app/api/create-portal-session/route.ts
@@ -4,7 +4,7 @@ import { supabaseAdmin } from "@/lib/supabase"
 import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-07-30.acacia"
+  apiVersion: "2025-07-30.basil"
 })
 
 export async function POST() {
